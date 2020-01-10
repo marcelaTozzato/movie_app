b45442b44093df9177377b6cd03eed389107c587
Marcela Limieri Tozzato <marcelaltozzato@gmail.com>
2020-01-10 17:14:04 -0300

    correções na cor e no titulo da navigation controller e correção na constrain da collection view

--- movie_app/movie_app/AppDelegate.swift
+++ movie_app/movie_app/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UITabBar.appearance().barTintColor = .customLigthYellow
         UITabBar.appearance().tintColor = .black
+        UINavigationBar.appearance().barTintColor = .customLigthYellow
         return true
     }
 
